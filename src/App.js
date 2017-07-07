97a2dc6f8b7db27e34e90df75b0cdab7bfde626c
Laura Do <laurado@Lauras-MacBook-Pro.local>
2017-07-07 14:39:28 -0400

    add button function

--- src/App.js
+++ src/App.js
@@ -4,44 +4,68 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pokemon: '',
-      name: '',
-      picture: ''
+      pokemon: ''
+      // name: '',
+      // picture: ''
     }
+    this.handleClick = this.handleClick.bind(this)
   }
 
   componentDidMount() {
     // let id = this.state.id
     // fetch(`/api/v1/bars/${id}`)
 
-    // let randomId = (Math.random() * 811)
-    let randomId = Math.floor(Math.random() * 811)+1
+    // let randomId = Math.floor(Math.random() * 802)+1
+    let randomId = Math.floor(Math.random() * 151)+1
     fetch(`http://pokeapi.co/api/v2/pokemon-form/${randomId}`)
     .then(response => response.json())
     .then(body => {
       console.log(body)
       this.setState({
-        name: body.name,
-        picture: body.sprites.back_default,
         pokemon: body
+        // name: body.name,
+        // picture: body.sprites.back_default
       })
     })
   }
 
-  render() {
-    // console.log(this.state.pokemon.name)
-    // console.log(this.state.pokemon.sprites.back_default) //sprites is undefined here
+  handleClick(event) {
+    // let randomId = Math.floor(Math.random() * 802)+1
+    let randomId = Math.floor(Math.random() * 151)+1
+    fetch(`http://pokeapi.co/api/v2/pokemon-form/${randomId}`)
+    .then(response => response.json())
+    .then(body => {
+      console.log(body)
+      this.setState({
+        pokemon: body
+      })
+    })
+    .then()
+  }
 
+  render() {
+
+    let picture;
+    let name;
+    if (
+      this.state.pokemon.sprites !== undefined &&
+      this.state.pokemon.name !== undefined
+    ) {
+      picture = this.state.pokemon.sprites.back_default
+      name = this.state.pokemon.name
+    } else {
+      picture = ''
+      name = ''
+    }
 
 
     return (
       <div className="row">
         <div className="small-6 small-centered columns text-center">
           <h1>Pokébutt!</h1>
-          {/* <img src="https://upload.wikimedia.org/wikipedia/en/3/39/Pokeball.PNG" alt=''/> */}
-          <img src={this.state.picture} alt=''/>
-          <h3>{this.state.name}</h3>
-          <button className="button">Get a Random Pokebutt</button>
+          <img src={picture} alt=''/>
+          <h3>{name}</h3>
+          <button className="button" onClick={this.handleClick}>Get a Random Pokebutt</button>
         </div>
       </div>
     );
